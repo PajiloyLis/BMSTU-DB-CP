2f2a46680cc15e8b4fcf3f38cc1b8f6432ed8e85
Bugakov Ivan <bugakov0601@yandex.ru>
2025-04-30 14:01:56 +0300

    create & drop

--- src/create.sql
+++ src/create.sql
@@ -1,39 +1,80 @@
 create table if not exists employee_base
 (
     id         uuid primary key default gen_random_uuid(),
-    full_name  text         not null,
-    phone      varchar(16) check ( phone ~ '^\+[0-9]{1,3}[0-9]{4,14}$' ),
-    email      varchar(255) not null check ( email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' ),
-    birth_date date         not null check ( birth_date < CURRENT_DATE ),
-    photo      text             default null,
+    full_name  text                not null,
+    phone      varchar(16) unique not null check ( phone ~ '^\+[0-9]{1,3}[0-9]{4,14}$' ),
+    email      varchar(255) unique not null check ( email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' ),
+    birth_date date                not null check ( birth_date < CURRENT_DATE ),
+    photo      text unique      default null,
     duties     jsonb            default null
 );
-
-
 
 create temporary view employee as
 select *, extract(year from age(employee_base.birth_date)) as age
 from employee_base;
 
-drop view employee;
-select *
-from employee;
+create table if not exists education
+(
+    id              uuid primary key default gen_random_uuid(),
+    employee_id     uuid references employee_base (id) on delete cascade,
+    institution     text not null,
+    education_level text not null,
+    study_field     text not null,
+    start_date      date not null check ( start_date < CURRENT_DATE ),
+    end_date        date not null check ( end_date <= CURRENT_DATE )
+);
 
-insert into employee_base(full_name, phone, email, birth_date)
-values ('Vasya', '+79991234567', 'vasyan@gmail.com', '12.01.2004'),
-       ('Kolya', '+78939876543', 'kolyan@yandex.ru', '23.06.1999');
+create table if not exists post
+(
+    id         uuid primary key default gen_random_uuid(),
+    title      text           not null,
+    salary     numeric(10, 2) not null check ( salary > 0 ),
+    company_id uuid references company (id) on delete cascade
+);
 
--- create table if not exists test(
---                                    created_at timestamptz default now()
--- );
---
--- create temporary view test_view as select *, now() - test.created_at as difference from test;
---
--- insert into test(created_at) values (now()), (now()), (now());
---
--- select * from test;
---
--- select * from test_view;
---
--- drop table test;
--- drop view test_view;
+create table if not exists company
+(
+    id                uuid primary key default gen_random_uuid(),
+    title             text         unique not null,
+    registration_date date         not null check ( registration_date <= CURRENT_DATE ),
+    phone             varchar(16)  unique not null check ( phone ~ '^\+[0-9]{1,3}[0-9]{4,14}$' ),
+    email             varchar(255) unique not null check ( email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' ),
+    inn               varchar(10)  unique not null check ( inn ~ '^[0-9]{10}$' ),
+    kpp               varchar(9)   unique not null check ( kpp ~ '^[0-9]{9}$' ),
+    ogrn              varchar(13)  unique not null check ( ogrn ~ '^[0-9]{13}$' ),
+    address           text         not null
+);
+-- TODO добавить каскадное обновление при удалении
+create table if not exists position
+(
+    id        uuid primary key default gen_random_uuid(),
+    parent_id uuid references position (id) on delete set default (select p.parent_id from position as p where p.id = position_id),
+    title     text not null
+);
+
+create table if not exists post_history
+(
+    post_id     uuid references post (id) on delete set null,
+    employee_id uuid references employee_base (id) on delete cascade,
+    start_date  date not null check ( start_date < CURRENT_DATE ),
+    end_date    date not null check ( end_date <= CURRENT_DATE )
+);
+
+create table if not exists position_history
+(
+    position_id uuid references position (id) on delete set null,
+    employee_id uuid references employee_base (id) on delete cascade,
+    start_date  date not null check ( start_date < CURRENT_DATE ),
+    end_date    date not null check ( end_date <= current_date )
+);
+
+create table if not exists score_story
+(
+    employee_id      uuid references employee_base (id) on delete cascade,
+    author_id        uuid references employee_base (id) on delete set null,
+    position_id      uuid references position (id) on delete set null ,
+    created_at       timestamptz not null default now(),
+    efficiency_score int check ( efficiency_score > 0 and efficiency_score < 6 ),
+    engagement_score int check ( engagement_score > 0 and engagement_score < 6 ),
+    competency_score int check ( competency_score > 0 and competency_score < 6 )
+);
---
+++ src/drop.sql
@@ -0,0 +1,17 @@
+drop view if exists employee;
+
+drop table if exists employee_base;
+
+drop table if exists education;
+
+drop table if exists post;
+
+drop table if exists company;
+
+drop table if exists position;
+
+drop table if exists post_history;
+
+drop table if exists position_history;
+
+drop table if exists score_story;
